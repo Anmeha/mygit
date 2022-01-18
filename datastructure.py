687567e5e8bc7a20934aaa2eb7e0f35e6344ab52
ankitamehare <ankita.mehare@1996@gmail.com>
2022-01-18 20:39:49 +0530

    lets add

---
+++ datastructure.py
@@ -0,0 +1,12 @@
+import random
+import pandas as pd
+
+L = ('1', 'a', '4', 'billie', '8', 'rajjo', '8')
+print(L)
+print(sorted(L))
+
+print(ord('A'))
+
+print("returns a random number from range(100) : ",random.choice(range(10)))
+
+pd.read_csv()
